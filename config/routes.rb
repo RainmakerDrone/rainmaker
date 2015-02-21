563ed6cecb9ad8cb3281dd6754c08cd3c7e3de18
brownrout <ericbrownrout2016@u.northwestern.edu>
2015-02-21 12:22:48 -0800

    add route for text

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   root 'twilio#index'
   post 'twilio/voice' => 'twilio#voice'
+  post 'twilio/text' => 'twilio#text'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
