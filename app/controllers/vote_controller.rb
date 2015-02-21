7fe5bc4e4ad47aa7a9e3cd44ae14d3419f1f8084
brownrout <ericbrownrout2016@u.northwestern.edu>
2015-02-21 14:25:26 -0800

    fleshing out the vote controller...

--- app/controllers/vote_controller.rb
+++ app/controllers/vote_controller.rb
@@ -1,7 +1,16 @@
 class VotesController < ApplicationController
 
+	def index
+		@votes = Vote.all
+	end
+
 	def new
       @vote = Vote.new
+	end
+
+	def create
+	  @vote = Vote.new(:name => parsed["From"], :description => parsed["Body"])
+	  @vote.save
 	end
 
 end
