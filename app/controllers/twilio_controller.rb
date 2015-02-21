d09e866484160438ec1f8f478f84c0f67075b180
brownrout <ericbrownrout2016@u.northwestern.edu>
2015-02-21 12:08:36 -0800

    remove token stuff

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -15,4 +15,14 @@ class TwilioController < ApplicationController
  
     render_twiml response
   end
+
+  def text 
+    # let's pretend that we've mapped this action to 
+    # http://localhost:3000/sms in the routes.rb file
+    
+    message_body = params["Body"]
+    from_number = params["From"]
+ 
+    SMSLogger.log_text_message from_number, message_body
+  end
 end
