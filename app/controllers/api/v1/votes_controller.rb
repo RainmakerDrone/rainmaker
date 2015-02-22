5c06ca5ac9983f1500f3af71ec88275ddf119dd9
brownrout <ericbrownrout2016@u.northwestern.edu>
2015-02-22 06:13:46 -0800

    updated the idea

--- app/controllers/api/v1/votes_controller.rb
+++ app/controllers/api/v1/votes_controller.rb
@@ -4,8 +4,8 @@ module Api
             respond_to :json
 
             def index
-                    #respond_with Vote.where(created_at: (Time.now-10.seconds)..(Time.now)).as_json
-                    respond_with Vote.all.as_json(:only => [:description])
+                    respond_with Vote.where(created_at: (Time.now-10.seconds)..(Time.now)).as_json
+                    #respond_with Vote.all.as_json(:only => [:description])
             end
             
         end
