8467798b9231a2b5ec905eeb6bb30da6c5d2f68b
brownrout <ericbrownrout2016@u.northwestern.edu>
2015-02-21 17:21:21 -0800

    missed the appropiate routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  namespace :api, defaults: {format: 'json'} do
+    namespace :v1 do
+        resources :votes
+    end
+  end
+
   root 'vote#index'
   post 'twilio/voice' => 'twilio#voice'
   post 'twilio/text' => 'twilio#text'
