44030e62421dd8576161dfcde483103be78c509a
brownrout <ericbrownrout2016@u.northwestern.edu>
2015-02-21 20:29:00 -0800

    temp update api to feed ALL of the votes

--- app/controllers/api/v1/votes_controller.rb
+++ app/controllers/api/v1/votes_controller.rb
@@ -4,7 +4,8 @@ module Api
             respond_to :json
 
             def index
-                    respond_with Vote.where(created_at: (Time.now-10.seconds)..(Time.now)).as_json
+                    #respond_with Vote.where(created_at: (Time.now-10.seconds)..(Time.now)).as_json
+                    respond_with Vote.all.as_json
             end
             
         end
